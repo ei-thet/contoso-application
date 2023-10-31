72310a4cc8a3755c1bb52632b9db8f1a144caac6
ei-thet <githubacc202308@gmail.com>
2023-10-31 14:52:28 +0000

    Modify main.tf variables.tf

--- dev/main.tf
+++ dev/main.tf
@@ -64,3 +64,102 @@ resource "aws_internet_gateway" "internet_gateway" {
     Name = "Terraform2023_internet_gateway"
   }
 }
+
+#3Public Subnets
+resource "aws_subnet" "public_subnet" {
+  count                   = 3
+  vpc_id                  = aws_vpc.contoso_dev_vpc.id
+  cidr_block              = var.public_subnet_cidr[count.index]
+  map_public_ip_on_launch = true
+  availability_zone       = data.aws_availability_zones.available.names[count.index]
+  tags = {
+    Name = join("-", ["${var.environment}-public-subnet", data.aws_availability_zones.available.names[count.index]])
+  }
+}
+
+#3Private Subnets
+resource "aws_subnet" "private_subnet" {
+  count             = 3
+  vpc_id            = aws_vpc.contoso_dev_vpc.id
+  cidr_block        = var.private_subnet_cidr[count.index]
+  availability_zone = data.aws_availability_zones.available.names[count.index]
+  tags = {
+    Name = join("-", ["${var.environment}-private-subnet", data.aws_availability_zones.available.names[count.index]])
+  }
+}
+
+/*Route table for public subnets, this ensures that all instances launched in 
+public subnet will have access to the internet*/
+resource "aws_route_table" "public_route_table" {
+  vpc_id = aws_vpc.contoso_dev_vpc.id
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.internet_gateway.id
+  }
+  tags = {
+    Name = "${var.environment}-public-route-table"
+  }
+}
+
+
+#Elastic IP 
+/*An EIP is a public IP address that can be assigned to an instance or load balancer. EIPs can be used to 
+make your instances accessible from the internet.*/
+resource "aws_eip" "elastic_ip" {
+  tags = {
+    Name = "${var.environment}-elastic-ip"
+  }
+}
+
+#AWS NAT Gateway:  
+/*is a network device that allows instances in a private subnet to access the internet. 
+It does this by translating the private IP addresses of the instances to public IP addresses.*/
+resource "aws_nat_gateway" "nat_gateway" {
+  allocation_id = aws_eip.elastic_ip.id
+  subnet_id     = aws_subnet.public_subnet[0].id
+  depends_on    = [aws_internet_gateway.internet_gateway]
+  tags = {
+    Name = "Terraform2023InternetGateway"
+  }
+}
+
+# Application Load Balancer Resources
+/*Creates an Application Load Balancer (ALB) that is accessible from the internet, uses the application load balancer 
+type, and uses the ALB security group. The ALB will be created in all public subnets.*/
+resource "aws_lb" "alb" {
+  depends_on         = [aws_autoscaling_group.auto_scaling_group]
+  name               = "${var.environment}-alb"
+  internal           = false
+  load_balancer_type = "application"
+  security_groups    = [aws_security_group.alb_security_group.id]
+  subnets            = [for i in aws_subnet.public_subnet : i.id]
+}
+
+#creating a target group that listens on port 80 and uses the HTTP protocol. 
+resource "aws_lb_target_group" "target_group" {
+  name     = "${var.environment}-tgrp"
+  port     = 80
+  protocol = "HTTP"
+  vpc_id   = aws_vpc.contoso_dev_vpc.id
+  health_check {
+    path    = "/"
+    matcher = 200
+  }
+}
+
+#Application Load Balancer: Is a powerful tool that can help you improve the performance, security, and 
+#availability of your applications
+/*Creating a listener that listens on port 80 and uses the HTTP protocol. The listener will be associated 
+with the application load balancer*/
+resource "aws_lb_listener" "alb_listener" {
+  load_balancer_arn = aws_lb.alb.arn
+  port              = "80"
+  protocol          = "HTTP"
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.target_group.arn
+  }
+  tags = {
+    Name = "${var.environment}-alb-listenter"
+  }
+}
--- dev/variables.tf
+++ dev/variables.tf
@@ -16,3 +16,17 @@ variable "environment" {
   type        = string
   # default     = "contoso-dev"
 }
+
+#These Public subnets are used for resources that need to be accessible from the internet
+variable "public_subnet_cidr" {
+  description = "Public Subnet cidr block"
+  type        = list(string)
+  default     = ["10.10.0.0/24", "10.10.2.0/24", "10.10.3.0/24"]
+}
+
+#These Private subnets can be used to deploy resources that do not need to be accessible from the internet.
+variable "private_subnet_cidr" {
+  description = "Private Subnet cidr block"
+  type        = list(string)
+  default     = ["10.10.4.0/24", "10.10.5.0/24", "10.10.6.0/24"]
+}
